a9825ccead7555c78e485b1141f2962ff3134e1a
JeanGoetten <34884262+Selbstdenker@users.noreply.github.com>
2019-08-11 11:42:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,76 +3,11 @@ var setTitle = require('console-title')
 var {table} = require('table') 
 var colors = require('colors')
 var binance = require('node-binance-api')().options({
-    APIKEY: config.API_KEY,
-    APISECRET: config.SECRET_KEY,
-    useServerTime: true
+    APIKEY: <key>,
+    APISECRET: <secret>,
+    useServerTime: true,
+    test: true
 })
-
-setInterval(()=>{
-    try {
-    binance.prices('TUSDUSDT', (error, ticker)=>{
-        setTitle('TUSD/USDT: ' + ticker.TUSDUSDT)
-    })
-    } catch (e){
-        throw e
-    }
-}, 1000)
-
-setInterval(function () {
-    var dateAtual = new Date()
-    try {
-        binance.balance((error, balances) => {
-            if (error) return console.error(error)
-            const total = parseFloat(balances.USDT.onOrder) + parseFloat(balances.USDT.available) + parseFloat(balances.TUSD.onOrder) + parseFloat(balances.TUSD.available)
-            console.clear()
-            console.log("==========================================")
-            console.log("SALDO TOTAL..:", total, "USD")
-            console.log("SALDO INICIAL:", config.INITIAL_INVESTMENT, "USD")
-            console.log("LUCRO........:", total - config.INITIAL_INVESTMENT, "USD")
-            dateAtual = new Date()
-            console.log("@", dateAtual.getHours() + ':' + dateAtual.getMinutes() + ':' + dateAtual.getSeconds())
-            console.log("==========================================")
-            var buy = config.BUY_PRICE
-            var sell = config.SELL_PRICE
-            try {
-                binance.prevDay("BTCUSDT", (error, prevDay, symbol) => {
-                    if (prevDay.priceChangePercent > 0) {
-                        buy = 0.9920
-                        sell = 0.9965
-                    } else {
-                        buy = 0.9965
-                        sell = 0.9995
-                    }
-                    console.log("Cotação BTC/USDT", prevDay.lastPrice)
-                    console.log("Definidos: compra " + buy + " e venda " + sell)
-                    console.log("==========================================")
-                    console.log(' ')
-
-                    if (balances.USDT.available > 20) {
-                        console.log("Compra: " + buy)
-                        try {
-                            binance.buy("TUSDUSDT", ((balances.USDT.available - 0.1) / buy).toFixed(2), buy)
-                        } catch (e) {
-                            throw e
-                        }
-                    }
-                    if (balances.TUSD.available > 20) {
-                        console.log("Venda: " + sell)
-                        try {
-                            binance.sell("TUSDUSDT", (balances.TUSD.available - 0.1).toFixed(2), sell)
-                        } catch (e) {
-                            throw e
-                        }
-                    }
-                })
-            } catch (e) {
-                throw e
-            }
-        });
-    } catch (e) {
-        console.log("ERRO : " + e)
-    }
-}, 20000)
 
 // var last_trade = [Date.now()]
 // var last_price = [1, 1]
